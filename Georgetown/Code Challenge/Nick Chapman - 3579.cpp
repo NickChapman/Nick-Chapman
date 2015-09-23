7b5b0e2cba5044f16f35b92b9aef7f96e3fbe5ad
Nick Chapman <nchapman2014@yahoo.com>
2015-09-23 17:19:05 -0400

    Changed folder names, added my code challenge solution

---
+++ Georgetown/Code Challenge/Nick Chapman - 3579.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+/*
+int main(){
+
+	while (true){
+		int n;
+		cin >> n;
+		if (n == 0){ break; }
+
+		vector<int> data(n);
+		vector<int> output;
+		for (int i = 0; i < n; i++){
+			cin >> data[i];
+		}
+
+		for (int i = n; i > 0; i--){
+			output.insert(output.begin() + data[i - 1], i);
+		}
+
+		//printing
+		for (int i = 0; i < n - 1; i++){
+			cout << output[i] << ",";
+		}
+		cout << output[n - 1] << endl;
+	}
+	return 0;
+}*/
